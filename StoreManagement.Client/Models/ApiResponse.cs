c54923f742c9c30cae597e15b9e0b0e513f57425
lac2304 <loihuu114@gmail.com>
2025-12-06 19:38:22 +0700

    xong supplier and promotion

--- StoreManagement.Client/Models/ApiResponse.cs
+++ StoreManagement.Client/Models/ApiResponse.cs
@@ -2,6 +2,8 @@ namespace StoreManagement.Client.Models
 {
     public class ApiResponse<T>
     {
-        public T Data { get; set; }
+        public int Code { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
     }
 }
